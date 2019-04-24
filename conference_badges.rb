0cc124d758c64116560486a799b9accae3c5c7ea
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-24 13:16:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(attendees)
   attendees.map {|name| badge_maker(name)}
 end
 
-def assign_rooms (attendees)
+def assign_rooms(attendees)
   room_assignment = 1
   attendees.collect do |name|
     return "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
