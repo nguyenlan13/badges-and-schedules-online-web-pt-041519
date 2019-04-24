5876498bdee77b4505f0769d1598c2f2519fbb2c
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-24 12:49:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|name| badge_maker(name)}
+end
+
+def assign_rooms (attendees)
+  attendees.each_with_index do |name, index|
+    return "Hello, #{name}! You'll be assigned to room #{index}!"
+end
