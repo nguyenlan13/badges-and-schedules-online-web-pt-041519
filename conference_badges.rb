ee6335728f3d7bfac878c4ae1a6fc5ad08a0aadc
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-24 13:20:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,6 @@ end
 
 def assign_rooms(attendees)
   room_assignment = 0
-  array =[]
   attendees.collect do |name|
     room_assignment +=1
     return ["Hello, #{name}! You'll be assigned to room #{room_assignment}!"]
